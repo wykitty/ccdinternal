25994b070829ad0a3101d00ac5314c3ebd9b386a
wykitty <waiyikitty@gmail.com>
2016-01-30 13:08:36 +0800

    add pid function

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@
 #include "libsc/lcd.h"
 //#include <kalman.h>
 
+//R11 C6 R22
+
 #define BLUE	0x001F
 #define CYAN	0x07FF
 #define BLACK	0x0000
@@ -75,14 +77,15 @@ FutabaS3010 *servo = 0;     //500-1300
 Tsl1401cl *ccd_up = 0;
 Tsl1401cl *ccd_down = 0;
 libsc::k60::JyMcuBt106 *bluetooth = 0;
-uint16_t mid_angle=1050; //[600,1500]
-uint16_t angle =500;
+uint16_t mid_angle = 900; //[500,1300]
+uint16_t angle = 500;
 Motor *mot=0;
 AlternateMotor *altmotor =0;
 std::array<uint16_t, Tsl1401cl::kSensorW> Data;
-std::array<uint16_t, Tsl1401cl::kSensorW> preprepreData;
+std::array<uint16_t, Tsl1401cl::kSensorW> copyData;
 std::array<uint16_t, Tsl1401cl::kSensorW> prepreData;
 std::array<uint16_t, Tsl1401cl::kSensorW> prevData;
+uint16_t copdata[Tsl1401cl::kSensorW]={0};
 uint16_t ccd_count;
 uint16_t speed;
 uint16_t errors, feedbkerror;
@@ -91,11 +94,15 @@ int xr[2] = {0,0};
 int xl[2] = {0,0};
 int errr[2]={0,0};
 double kp,ki,kd;
+bool lflag, rflag;
+uint16_t lline, rline;
 uint16_t color = 0xFFFF;
 uint16_t mid;
 uint16_t color2 = 0xFFE0;
 uint16_t feedback, preError;
 int16_t sumerror=0;
+int16_t prev_err_val = 0;
+int16_t dt=0, integral_val=0;
 int16_t error=0;
 int16_t preverror=0;
 
@@ -112,9 +119,13 @@ void changespeed(uint16_t);
 void configuration(void);
 void ccdbuffer(void);
 void check_joystick(void);
+void coppydata(void);
 void checkangle(void);
+uint16_t otsu(uint16_t *);
+double calculate_error(void);
 void speedPID(void);
 void turnPID(void);
+void PID(int16_t, int16_t);
 void detmid(void);
 void ledge(void);
 void redge(void);
@@ -151,16 +162,21 @@ int main(void)
 	kd = 0;
 
 	while(1){
+		leda.SetEnable(1);
+		//writ->WriteString("haha");
+
 		if(t != System::Time()){
 			t=System::Time();
+			//leda.SetEnable(1);
 			//sprintf(buffer, "%d",System::Time());
+
 
 			if (t%10==0){
 				//check_joystick();
 				ccd_up->StartSample();
 				while(!ccd_up->SampleProcess()){};
 				Data = ccd_up->GetData();
-//				sprintf(ccd_buf, "%d", Data[64]);
+//				sprintf(ccd_buf, "%d", ledge);
 //				writ->WriteString("midno:");
 //				writ->WriteString(ccd_buf);
 
@@ -171,10 +187,19 @@ int main(void)
 //				if(ccd_count<3){
 //					ccd_count+=1;
 //				}
+				for(uint16_t i=0; i<Tsl1401cl::kSensorW; i++){
+					lcd->SetRegion(Lcd::Rect(i, (255-Data[i])/2.0,1,1));
+					lcd->FillPixel(&color,1);
+				}
 				ledge();
 				redge();
 				detmid();
-				turnPID();
+				PID();
+				sprintf(ano, "%d", errl[0]);
+				writ->WriteString(ano);
+				sprintf(ccd_buf, "%d", errr[0]);
+				writ->WriteString(ccd_buf);
+
 				/*
 				for (uint16_t q=0; q<Tsl1401cl::kSensorW; q++){
 					Data[q] = (prevData[q]*0.3 + Data[q]*0.7);
@@ -186,8 +211,6 @@ int main(void)
 					lcd->SetRegion(Lcd::Rect(i, (255-Data[i])/2.0,1,1));
 					lcd->FillPixel(&color,1);
 				}
-
-				//std::copy(Data, Data+Tsl1401cl::kSensorW, prevData);
 
 				/*
 				if(ccd_count<3){
@@ -207,6 +230,7 @@ int main(void)
 					prevData = Data;
 				}
 				*/
+
 				System::DelayMs(5);
 
 				for(uint16_t i=0; i<Tsl1401cl::kSensorW; i++){
@@ -214,12 +238,17 @@ int main(void)
 					lcd->FillColor(libsc::Lcd::kBlack);
 				}
 			}
+
+			//leda.SetEnable(1);
+
 			if(t%1000 == 0){
-				console->Clear(1);
-				leda.Switch();
+				//console->Clear(1);
+				//leda.SetEnable(1);
+				//leda.Switch();
 				ledb.Switch();
 			}
 		}
+
 	}
 	return 0;
 }
@@ -284,9 +313,6 @@ void print_info(uint16_t color2){
 	}
 }
 
-void ccdbuffer(void){
-
-}
 
 void check_joystick(void){
 	if(!ispressed){
@@ -330,28 +356,82 @@ void checkangle(void){
 	angle = (angle < 500) ? 500 : (angle > 1250) ? 1250 : angle;
 }
 
+void coppydata(void){
+	for(uint16_t i=0; i<Tsl1401cl::kSensorW; i++){
+		copdata[i]=Data[i];
+	}
+}
+
+void ccdbuffer(void){
+	prevData=Data;
+
+}
+
+
+uint16_t otsu(uint16_t *p){
+	uint32_t g,max=0;
+	uint16_t tt=0, ttlow=0;
+	uint8_t u0=0, u1=0, count=0, cnt=0;
+	uint16_t tr=0;
+	uint8_t pc[256] = {0};
+	uint8_t j;
+	for(j=5; j<=122; j++){
+		pc[*(p+j)]++;
+		tt+=*(p+j);
+	}
+	for(j=0; j<=254; j++){
+		cnt=pc[j];
+		if(cnt==0) continue;
+		count+=pc[j];
+		ttlow+=cnt*j;
+		u0=ttlow/count;
+		u1=(tt-ttlow)/(118-count);
+		g=((uint32_t)(u0-u1)*(u0-u1))*((count*(118-count)))/16384;
+		if(g>max){
+			max=g;
+			tr=j;
+		}
+		if(count>=118) break;
+	}
+	return tr;
+}
+
 
 void ledge(void){
+	lflag=0;
 	errl[0]=0;
-	//char edg[30];
-	for(uint16_t i=Tsl1401cl::kSensorW/2; i<Tsl1401cl::kSensorW; i++){
-		int j= i+4;
+	int j,k,l,m;
+	char edg[30];
+	for(uint16_t i=0; i<Tsl1401cl::kSensorW/2; i++){
+		if ((Data[i] + 4)<= Data[i+1]){
+			if((Data[i+1]+4) <= Data[i+2]){
+				if((Data[i+2]+4) <= Data[i+3]){
+					coppydata();
+					uint16_t mline = otsu(copdata);
+					if(Data[i+3]-Data[i]>= mline){
+						lflag = true;
+					}
+				}
+			}
+		}
 		errl[1]=errl[0];
+		j=i+4;
 		xl[1]=j;
 		if((Data[j]-Data[i])>errl[0]){
 			errl[0] = Data[j]-Data[i];
 			xl[0]=j-2;
 		}
 	}
-	//sprintf(edg, "%d",xl[0]);
-	//console->WriteString("left:");
-	//console->WriteString(edg);
+	if(lflag==true){
+		lline=xl[0];
+	}
+
 }
 
 void redge(void){
 	errr[0]=0;
 	char edg[30];
-	for(uint16_t i=0; i<Tsl1401cl::kSensorW/2; i++){
+	for(uint16_t i= Tsl1401cl::kSensorW/2; i<Tsl1401cl::kSensorW; i++){
 		int j = i+4;
 		errr[1]=errr[0];
 		xr[1]=j;
@@ -360,9 +440,7 @@ void redge(void){
 			xr[0]=j+2;
 		}
 	}
-	//sprintf(edg, "%d", xr[0]);
-	//console->WriteString("right:");
-	//console->WriteString(edg);
+
 }
 
 void detmid(void){
@@ -372,13 +450,65 @@ void detmid(void){
 	//console->WriteString(mil);
 }
 
-void turnPID(void){
+double calculate_error(void){
+	uint16_t ccd_min_val, ccd_max_val;
+
+	ccd_min_val = ccd_max_val = Data[0];
+
+	// Find out the upper and lower boundary of current batch of the signal.
+	for(int i = 1; i < Tsl1401cl::kSensorW; i++) {
+		if(Data[i] > ccd_max_val)
+			ccd_max_val = Data[i];
+		if(Data[i] < ccd_min_val)
+			ccd_min_val = Data[i];
+	}
+
+	uint16_t threshold = (ccd_min_val + ccd_max_val) / 2;
+
+	int left_pos = -1, right_pos = -1;
+	bool state = (Data[0] < threshold);
+
+	for(int i = 1; i < Tsl1401cl::kSensorW; i++) {
+		if(state ^ (Data[i] < threshold)) {
+			// State change.
+			// Note: Record the first change as left side,
+			//        while the last change as right side.
+			if(left_pos == -1)
+				left_pos = i;
+			else
+				right_pos = i;
+		}
+	}
+	return (left_pos + right_pos) / 2.0;
+}
+
+
+void PID(int16_t target_val, int16_t curr_val){
+	//Error
+	int16_t curr_err_val = target_val - curr_val;
+
+	//Proportional
+	int16_t p_out = kp * error;
+
+	//Integral
+	integra_val += curr_err_val * dt;
+	int16_t i_out = ki * su;
+
+	//Derivative
+	int16_t derivative = (curr_err_val - prev_err_val) / dt;
+	int16_t d_out = kd * dervivative;
+
+	//Calculate total output
+	int16_t output = p_out + i_out + d_out;
+
+	//Check power
+	changespeed(output);
+
 	kp=0.6;
 	ki=0.2;
-	sumerror=sumerror+error;
-	char ang[30];
+
 	uint16_t turn = angle;
-	error= mid-Tsl1401cl::kSensorW/2;
+
 	//preverror=
 	if(mid>64){
 		turn = 900 + (uint16_t)(kp*error + ki*sumerror + kd*(preverror-error));
@@ -417,7 +547,6 @@ void turn(void){
 void speedPID(void){
 
 }
-
 
 void changespeed(uint16_t speed){
 	speed = (speed > 400) ? 400: speed;
@@ -502,4 +631,3 @@ void configuration(void){
 	bluetooth = new libsc::k60::JyMcuBt106(bluetooth_config);
 	*/
 }
-
